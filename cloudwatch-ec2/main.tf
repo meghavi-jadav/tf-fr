67b0a63d1234667a8f7905d15cedf643ef5d5680
Meghavi Jadav <mjadav@parkar.digital>
2025-06-02 14:39:13 +0530

    Create main.tf

---
+++ cloudwatch-ec2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+provider aws {
+    region = "us-east-1"
+}
+
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 300
+  statistic                 = "Average"
+  threshold                 = 70
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
